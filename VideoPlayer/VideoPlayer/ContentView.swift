3930e79c307e3ad4a016adf3bc7e22b6307b30a2
Jun Hyeop <limjunehyeop@gmail.com>
2023-01-09 21:19:16 +0900

    Add url feature

--- VideoPlayer/VideoPlayer/ContentView.swift
+++ VideoPlayer/VideoPlayer/ContentView.swift
@@ -6,15 +6,19 @@
 //
 import SwiftUI
 import AVKit
+import AVFoundation
 
 struct ContentView: View{
     @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
-    @State var gradient1 = AngularGradient(gradient: Gradient(colors: [Color.purple, Color.blue]),
+    @State var gradient1 = AngularGradient(gradient: Gradient(colors: [Color.purple, Color.red]),
                                            center: .topTrailing, angle: .degrees(180+100))
-    @State var gradient2 = AngularGradient(gradient: Gradient(colors: [Color.purple, Color.red]),
+    @State var gradient2 = AngularGradient(gradient: Gradient(colors: [Color.green, Color.blue]),
+                                           center: .topTrailing, angle: .degrees(180+100))
+    @State var gradient3 = AngularGradient(gradient: Gradient(colors: [Color.purple, Color.blue]),
                                            center: .topTrailing, angle: .degrees(180+100))
     @AppStorage("_viewFolder") var viewFolder: Bool = false
     @AppStorage("_viewFile") var viewFile: Bool = false
+    @AppStorage("_viewLink") var viewLink: Bool = false
     @AppStorage("_directoryName") var dirName: String="unset"
     var body: some View{
         NavigationView{
@@ -22,35 +26,51 @@ struct ContentView: View{
                 ZStack{
                     
                     RoundedRectangle(cornerRadius: 20)
-                                .fill(gradient2)
+                                .fill(gradient1)
                                 .hoverEffect(.lift)
-                        .frame(width: 340, height: 280)
-                    Text("Open File")
-                        .font(.title)
-                        .fontWeight(.bold)
+                        .frame(width: 340, height: 180)
+                   VStack {
+                        Text("Open File")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("Play video directly with file")
+                            .foregroundColor(.secondary)
+                    }
                 }.onTapGesture {
                     viewFile.toggle()
                 }
                 ZStack{
                     RoundedRectangle(cornerRadius: 20)
-                                .fill(gradient1)
+                                .fill(gradient2)
                                 .hoverEffect(.lift)
-                        .frame(width: 340, height: 280)
-                    if dirName == "unset"{
+                        .frame(width: 340, height: 180)
+                    VStack {
                         Text("Open Directory")
-                            .font(.title)
-                            .fontWeight(.bold)
-                    } else{
-                        Text(dirName)
-                            .font(.title)
-                            .fontWeight(.bold)
+                               .font(.title)
+                               .fontWeight(.bold)
+                        Text("Play video from directory")
+                            .foregroundColor(.secondary)
                     }
                     
                 }.onTapGesture {
                     viewFolder.toggle()
                 }
-                .padding(.bottom)
-                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                                .fill(gradient3)
+                                .hoverEffect(.lift)
+                        .frame(width: 340, height: 180)
+                    VStack {
+                        Text("Open Link")
+                               .font(.title)
+                               .fontWeight(.bold)
+                        Text("Stream video from link")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }.onTapGesture {
+                    viewLink.toggle()
+                }
             }.navigationBarTitle("Welcome")
                 .navigationBarItems(trailing:NavigationLink(destination: SettingsView(isFirstLaunching: $isFirstLaunching), label: {Image(systemName: "gearshape.fill").foregroundColor(.primary)}))
                 .fullScreenCover(isPresented: $isFirstLaunching) {
@@ -60,26 +80,13 @@ struct ContentView: View{
                     FileView()
                 }.fullScreenCover(isPresented: $viewFolder) {
                     FolderView()
+                }.fullScreenCover(isPresented: $viewLink) {
+                    LinkView()
                 }
         }
     }
 }
-struct SelectionView: View{
-    var body: some View{
-        TabView{
-            FolderView()
-                .tabItem{
-                        Image(systemName: "externaldrive.fill")
-                        Text("Folder")
-            }
-            FileView()
-                .tabItem{
-                        Image(systemName: "folder.fill")
-                        Text("File")
-            }
-        }.accentColor(.black)
-    }
-}
+
 struct FolderView: View {
     @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
     @AppStorage("_directoryName") var dirName: String="unset"
@@ -114,7 +121,7 @@ struct FolderView: View {
                     Button(action: {showPicker.toggle()}, label: {HStack{Image(systemName: "plus.circle.fill");Text("Change Directory")}})
                 }
             }
-            .navigationTitle("Home")
+            .navigationTitle("Play with folder")
             .navigationBarItems(leading: Button(action: {viewFolder.toggle()}, label: {
                 Image(systemName: "xmark.circle.fill")
                 .foregroundColor(.primary)}))
@@ -140,6 +147,7 @@ struct FolderView: View {
         }
     }
 }
+
 struct FileView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
@@ -176,7 +184,7 @@ struct FileView: View {
             .fullScreenCover(isPresented: $isFirstLaunching) {
                 OnboardingTabView(isFirstLaunching: $isFirstLaunching)
             }
-            .navigationTitle("Home")
+            .navigationTitle("Play with file")
             .navigationBarItems(leading: Button(action: {viewFile.toggle()}, label: {
                 Image(systemName: "xmark.circle.fill")
                 .foregroundColor(.primary)}))
@@ -203,7 +211,90 @@ struct FileView: View {
     }
 }
 
+struct LinkView: View {
+    @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
+    @AppStorage("_viewLink") var viewLink: Bool = false
+    @State var url: String = ""
+    @State var validVideo: Bool = false
+    @State var unvalidVideo: Bool = false
+    var body: some View {
+        NavigationView{
+            VStack{
+                HStack{
+                    TextField("URL", text: $url)
+                        .padding()
+//                        .foregroundColor(Color("scheme"))
+                        .overlay(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.primary, lineWidth: 3)
+                                )
+                        .padding(.leading)
+                    if url != ""{
+                        if validateURL(urlString: url) == false{
+                            Button(action: {}, label: {Image(systemName: "play.circle.fill")})
+                                .foregroundColor(Color("scheme"))
+                                .padding()
+                                .background(.secondary)
+                                .cornerRadius(15)
+                                .padding(.trailing)
+                                .disabled(true)
+                        } else {
+                            Button(action: {
+                                let asset = AVAsset(url: URL(string: url)!)
+                                if asset != nil {
+                                    validVideo.toggle()
+                                } else {
+                                    unvalidVideo.toggle()
+                                }
+                            }, label: {Image(systemName: "play.circle.fill")})
+                            .alert(isPresented: $validVideo){
+                                Alert(title: Text("Valid video"), dismissButton: .default(Text("Dismiss")))
+                            }.alert(isPresented: $unvalidVideo){
+                                Alert(title: Text("Unvalid video"), dismissButton: .default(Text("Dismiss")))
+                            }
+                                .foregroundColor(Color("scheme"))
+                                .padding()
+                                .background(.primary)
+                                .cornerRadius(15)
+                                .padding(.trailing)
+                                
+                        }
+                        
+                    }else {
+                        Button(action: {}, label: {Image(systemName: "play.circle.fill")})
+                            .foregroundColor(Color("scheme"))
+                            .padding()
+                            .background(.secondary)
+                            .cornerRadius(15)
+                            .padding(.trailing)
+                            .disabled(true)
+                            
+                    }
 
+                    
+                        
+                }
+            }
+            .navigationTitle("Stream from link")
+            .navigationBarItems(leading: Button(action: {viewLink.toggle()}, label: {
+                Image(systemName: "xmark.circle.fill")
+                .foregroundColor(.primary)}))
+            .navigationBarItems(trailing:NavigationLink(destination: SettingsView(isFirstLaunching: $isFirstLaunching), label: {Image(systemName: "gearshape.fill").foregroundColor(.primary)}))
+            .fullScreenCover(isPresented: $isFirstLaunching) {
+                OnboardingTabView(isFirstLaunching: $isFirstLaunching)
+            }
+        }
+        
+    }
+    func validateURL (urlString: String?) -> Bool {
+        if let urlString = urlString {
+            if let url = NSURL(string: urlString) {
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
+        }
+        return false
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- VideoPlayer/VideoPlayer/settings.swift
+++ VideoPlayer/VideoPlayer/settings.swift
@@ -7,16 +7,21 @@ import SwiftUI
 
 struct SettingsView: View{
     @AppStorage("_directoryName") var dirName: String="unset"
-
+    @AppStorage("_viewFolder") var viewFolder: Bool = false
+    @AppStorage("_viewFile") var viewFile: Bool = false
     @Binding var isFirstLaunching: Bool
-    @State var showAuthor: Bool = false
+    @AppStorage("_showAuthor") var showAuthor: Bool = false
+    @State var reset: Bool = false
     var body: some View{
             Form {
                 Button(action: {showAuthor = true}, label: {Text("Who made this?")})
                     .sheet(isPresented: $showAuthor) {
                         AuthorView()
-                    }
-                Button(action: {isFirstLaunching = true;dirName="unset"}, label: {Text("Reset").foregroundColor(.red)})
+            }
+                Button(action: {isFirstLaunching.toggle();dirName="unset";viewFile=false;viewFolder=false;reset.toggle()}, label: {Text("Reset").foregroundColor(.red)})
+            }.onAppear(perform: {showAuthor=false})
+            .fullScreenCover(isPresented: $reset){
+                ContentView()
             }
             .navigationBarTitle("Settings")
             
@@ -24,14 +29,13 @@ struct SettingsView: View{
 }
     
 struct AuthorView: View{
+    @AppStorage("_showAuthor") var showAuthor: Bool = true
     var body: some View{
         NavigationView{
             VStack{
                 Image(systemName: "video.fill")
                     .resizable()
                     .frame(width: 100.0, height: 63)
-                    
-                
                 Text("VPlayer")
                     .font(.title)
                     .fontWeight(.bold)
@@ -43,6 +47,12 @@ struct AuthorView: View{
                     Image(systemName: "person.crop.circle.fill")
                 }
             }.navigationBarTitle("Author")
+                .navigationBarItems(trailing: Button(action: {showAuthor=false}, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.primary)
+                        .padding(.top, 100.0)
+                        
+            }))
         }
     }
 }
